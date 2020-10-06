0246a11cdc34747ab9c323bfb8c8addad5c55b01
practise97 <72223064+practise97@users.noreply.github.com>
2020-10-06 18:12:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = name + " likes to eat Mangos"
+	new_name = str(name) + " likes to eat Mangos"
 	return render_template('index.html', name=new_name)
 
 
